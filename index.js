3090d272bfe45ebadbfe33fff99e8ea450f03d4d
vinyborges <viny.bdj@gmail.com>
2022-12-06 08:59:32 -0800

    commit tree fd13758741ccdc1a86b3b620417dd9f1a9a243a8

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+document.getElementById("main")
+main.remove()
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "Vinicius is the champion"
+
+
+
+
+
+
+
